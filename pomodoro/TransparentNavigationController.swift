56d4da6f831d0fd8590812af866db03209783137
Frédéric Torcheux <frederic.torcheux@djit.fr>
2020-08-29 12:12:29 +0200

    [NavigationBar] Fix transparent color for navigation bar

--- pomodoro/TransparentNavigationController.swift
+++ pomodoro/TransparentNavigationController.swift
@@ -1,31 +1,28 @@
 import UIKit
 
 class TransparentNavigationController: UINavigationController {
-
+    
     private struct Constants {
         static let tintColor = UIColor.white
     }
-
+    
     override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
+        setupTransparentNavigationBar()
     }
-
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private func setupTransparentNavigationBar() {
         self.navigationBar.tintColor = Constants.tintColor
-
         self.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationBar.shadowImage = UIImage()
         self.navigationBar.isTranslucent = true
     }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-
 }
